c0350362af2531749d6b8f865c163afc3e13ce72
Dylan Ede <dylanede@googlemail.com>
2015-01-30 18:03:53 +0000

    Updated build script to support cross-compilation.

--- cef-sys/build.rs
+++ cef-sys/build.rs
@@ -3,9 +3,40 @@
 use std::os;
 use std::old_io::fs::PathExtensions;
 
-#[cfg(target_os="macos")]
+enum Platform {
+    Windows,
+    Mac,
+    Linux
+}
+
+fn get_platform() -> Platform {
+    match os::getenv("TARGET").unwrap().split('-').nth(2).unwrap() {
+        "win32" | "windows" => Platform::Windows,
+        "darwin" => Platform::Mac,
+        "linux" => Platform::Linux,
+        other => panic!("Sorry, platform \"{}\" is not supported by cef-sys.", other)
+    }
+}
+
 fn main() {
-    /*
+    let dll_name = match get_platform() {
+        Platform::Mac => return, // CEF_PATH is not necessarily needed for Mac
+        Platform::Windows => "libcef",
+        Platform::Linux => "cef"
+    };
+    let dll_file_name = os::dll_filename(dll_name);
+    let cef_dir = Path::new(os::getenv("CEF_PATH")
+        .expect("CEF_PATH needs to point to the directory containing the CEF DLL."));
+    let cef_path = cef_dir.join(dll_file_name.clone());
+    if !cef_path.exists() {
+        panic!("Unable to find {} in {}", dll_file_name, cef_dir.as_str().unwrap());
+    }
+    println!("cargo:rustc-flags=-l {} -L {}", dll_name, cef_dir.as_str().unwrap());
+}
+
+/*
+fn main() {
+    
     let cef_dir = os::getenv("CEF_PATH")
         .expect("CEF_PATH needs to point to the directory containing the CEF DLL.");
 
@@ -18,37 +49,6 @@ fn main() {
         panic!("Unable to find {} in {}", cef_lib, cef_dir.as_str().unwrap());
     }
     println!("cargo:rustc-flags= -L {}",
-             cef_dir.as_str().unwrap());*/
+             cef_dir.as_str().unwrap());
 }
-
-#[cfg(target_os="linux")]
-fn main() {
-    let cef_dir = os::getenv("CEF_PATH")
-        .expect("CEF_PATH needs to point to the directory containing the CEF DLL.");
-
-    let cef_dir = Path::new(cef_dir);
-
-    let cef_lib = os::dll_filename("cef");
-
-    let cef_path = cef_dir.join(cef_lib.clone());
-    if !cef_path.exists() {
-        panic!("Unable to find {} in {}", cef_lib, cef_dir.as_str().unwrap());
-    }
-    println!("cargo:rustc-flags=-l cef -L {}", cef_dir.as_str().unwrap());
-}
-
-#[cfg(target_os="windows")]
-fn main() {
-    let cef_dir = os::getenv("CEF_PATH")
-        .expect("CEF_PATH needs to point to the directory containing the CEF DLL.");
-
-    let cef_dir = Path::new(cef_dir);
-
-    let cef_lib = os::dll_filename("libcef");
-
-    let cef_path = cef_dir.join(cef_lib.clone());
-    if !cef_path.exists() {
-        panic!("Unable to find {} in {}", cef_lib, cef_dir.as_str().unwrap());
-    }
-    println!("cargo:rustc-flags=-l libcef -L {}", cef_dir.as_str().unwrap());
-}
+*/
