7b1c7cd700321e3625537d0f645264342a0d19e1
anderejd <rajder@gmail.com>
2017-05-20 18:34:05 +0200

    Using syn and quote crates for proper TokenStream processing.

--- extern_attrib/src/lib.rs
+++ extern_attrib/src/lib.rs
@@ -1,10 +1,45 @@
+//! TODO: Study and do proper procedural macro error handling.
 #![feature(proc_macro)]
 
 extern crate proc_macro;
+extern crate syn;
+
+#[macro_use]
+extern crate quote;
 
 use proc_macro::TokenStream;
 use std::str::FromStr;
+use syn::ItemKind;
 
+#[proc_macro_attribute]
+pub fn extern_auto(_: TokenStream, input: TokenStream) -> TokenStream {
+    let src = input.to_string();
+    let item = syn::parse_item(&src);
+    let mut item = match item {
+        Ok(i) => i,
+        Err(_) => {
+            // Ignore syntax errors and let the compiler handle it.
+            eprintln!("Parse error in extern_auto.");
+            return input
+        }
+    };
+    {
+        let abi_opt = match item.node {
+            ItemKind::Fn(_, _, _, ref mut opt, _, _) => opt,
+            _ => return input
+        };
+        modify_abi(abi_opt);
+    }
+    let tokens = quote!(#item).to_string();
+    println!("tokens: {}", tokens);
+    TokenStream::from_str(&tokens).unwrap() // TODO: Handle error.
+}
+
+fn modify_abi(abi: &mut Option<syn::Abi>) {
+    let name = get_platform_abi_name();
+    *abi = Some(syn::Abi::Named(name.into()));
+}
+    
 #[cfg(target_os = "windows")]
 fn get_platform_abi_name() -> &'static str {
     //! TODO: Investigate if rustc interprets "stdcall" as "C" for x64.
@@ -19,12 +54,4 @@ fn get_platform_abi_name() -> &'static str {
 #[cfg(not(any(target_os = "windows", target_os = "macos", target_os = "linux")))]
 fn get_platform_abi_name() -> &'static str {
     panic!("Platform not supported.");
-}
-
-#[proc_macro_attribute]
-pub fn extern_auto(_: TokenStream, input: TokenStream) -> TokenStream {
-    //! TODO: Study and do proper procedural macro error handling.
-    let abi = get_platform_abi_name();
-    let s = format!("extern \"{}\" {}", abi, input);
-    TokenStream::from_str(&s).unwrap()
 }
--- extern_attrib/tests/tests.rs
+++ extern_attrib/tests/tests.rs
@@ -8,16 +8,17 @@ mod tests {
     #[test]
     fn extern_auto_resolves_as_expected() {
         #[extern_auto]
+        #[cfg(not(extra_attrib_to_make_things_harder = "for the procedural macro"))]
         fn modified_by_attrib() -> i32 {
             1 + 2
         }
 
         /// Expect build failure if extern_auto is incorrect.
-        #[cfg(windows)]
+        #[cfg(target_os = "windows")]
         let f: extern "stdcall" fn() -> i32 = modified_by_attrib;
 
         /// Expect build failure if extern_auto is incorrect.
-        #[cfg(not(windows))]
+        #[cfg(not(target_os = "windows"))]
         let f: extern "C" fn() -> i32 = modified_by_attrib;
 
         //let i = modified_by_attrib();
